1591ee7fb6ac868f1b231ae7495be817e51d1c3b
Payal Lakra <payallakra307@gmail.com>
2025-09-23 20:11:53 +0530

    SQL_Advanced

---
+++ Day13/Assignment3.sql
@@ -0,0 +1,20 @@
+select * from Customers;
+
+select * from Orders;
+
+select * from Products;
+
+
+-- 1) Write an SQL query to find the names of customers who have placed an order.
+
+-- 2) Find the list of customers who have not placed any orders.
+
+-- 3) List all orders along with the product name and price. 
+
+-- 4) Find the names of customers and their orders, including customers who haven't placed any orders.
+
+-- 5) Retrieve a list of products that have never been ordered.
+
+-- 6) Find the total number of orders placed by each customer.
+
+-- 7) Display the customers, the products they've ordered, and the order date. Include customers who haven't placed any orders.
